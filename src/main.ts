3a24f0f979822f51657a03e75941bd1fc326a2db
TOM LUCAS <tom.lucas@viacesi.fr>
2022-11-28 12:34:23 +0100

    ✨ add accessibility checker

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,26 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import accessibilityColorSelector from './app/shared/utils';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+onload = (event) => {
+  for (let element of Array.from(document.querySelectorAll('.accessibilityCheck'))) {
+    try{
+      const relatedImg = Array.of(document.querySelector('.accessibilityCheckImg')!)
+        .filter((img) => (img as HTMLElement).dataset['num'] === (element as HTMLElement).dataset['num'])[0] as HTMLImageElement;
+      (element as HTMLElement).style.color = accessibilityColorSelector({
+        imageLink: relatedImg.src, position: (element as HTMLElement).dataset['position'] });
+    } catch(e) {
+      console.error(e);
+    }
+  }
+};
+
+
+
+
+
